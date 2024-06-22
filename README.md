83230e1b2a6d25326720edf6b24b7736b58eeff0
LEADER <godstark82@gmail.com>
2024-06-22 20:47:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flixvibes_website
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flixvibes_website/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flixvibes_website
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
